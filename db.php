6c798ee6f99552d7b841b99924de68aa21fecd61
Rayan Toufella <rayantoufella3@gmail.com>
2026-03-13 10:47:31 +0000

    first connection with db PDO mysql

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php 
+$host='localhost' ; 
+$username = 'root';
+$password = '';
+$db_name = 'Challenge1';
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name",$username,$password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    echo"connection !";
+
+}catch(Exception $e){
+    die("Error Could not be Connected ". $e->getMessage());
+}
+
+
+
